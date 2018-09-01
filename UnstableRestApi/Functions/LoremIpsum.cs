c0638dc545eabd9ec045284da17160171eeedaa6
Lloyd Atkinson <lloydjatkinson@gmail.com>
2018-09-02 00:53:14 +0100

    Clean up code and reduce maximum delay to 3 seconds

--- UnstableRestApi/Functions/LoremIpsum.cs
+++ UnstableRestApi/Functions/LoremIpsum.cs
@@ -1,11 +1,11 @@
-
-using System;
 using System.Threading.Tasks;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
+
 using UnstableRestApi.Services;
 
 namespace UnstableRestApi
@@ -15,7 +15,7 @@ namespace UnstableRestApi
         [FunctionName("lorem")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, ILogger log)
         {
-            await RandomDelayHelper.DelayRandomly(5);
+            await RandomDelayHelper.DelayRandomly(3);
 
             log.LogInformation($"{nameof(LoremIpsum)} function triggered.");
 
