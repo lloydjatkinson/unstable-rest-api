858ad31282792c6b75d32f94ef3b5c3374a534e7
Lloyd Atkinson <lloydjatkinson@gmail.com>
2018-09-03 22:13:32 +0100

    Clean up comments

--- UnstableRestApi/Functions/LoremIpsum.cs
+++ UnstableRestApi/Functions/LoremIpsum.cs
@@ -12,24 +12,14 @@ namespace UnstableRestApi
 {
     public static class LoremIpsum
     {
-        [FunctionName("lorem")]
+        [FunctionName("loremipsum")]
         public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, ILogger log)
         {
+            log.LogInformation($"{nameof(LoremIpsum)} function triggered.");
             await RandomDelayHelper.DelayRandomly(3);
 
-            log.LogInformation($"{nameof(LoremIpsum)} function triggered.");
 
             return ResponseFactory.LoremIpsum(10);
-
-            //string name = req.Query["name"];
-
-            //string requestBody = new StreamReader(req.Body).ReadToEnd();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //name = name ?? data?.name;
-
-            //return name != null
-            //    ? (ActionResult)new OkObjectResult($"Hello, {name}")
-            //    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
 }
